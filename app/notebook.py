2ae82ae41574f3cd0718602907c40d2a25e883ae
Camila Pradilla <Mariacamillapra18@gmail.com>
2026-02-16 17:04:14 -0500

    add_tag commit

--- app/notebook.py
+++ app/notebook.py
@@ -1,4 +1,5 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+import datetime
 from idlelib.configdialog import HighPage
 
 
@@ -7,4 +8,16 @@ class Note:
     MEDIUM: str = "MEDIUM"
     LOW: str = "LOW"
 
+    def __init__(self, code: str, title: str, text: str, importance: str):
+        self.code = code
+        self.title = title
+        self.text = text
+        self.importance = importance
+        self.creation_date = datetime.now()
+        self.tags: list[str] = []
+
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
 
