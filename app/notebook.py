d366bafa63181e189caaae1fbb6219e85b414de0
carl1347 <carlosotalor132005@gmail.com>
2026-02-16 16:34:19 -0500

    Constante commit

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,10 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+from idlelib.configdialog import HighPage
+
+
+class Note:
+    HIGH: str = "HIGH"
+    MEDIUM: str = "MEDIUM"
+    LOW: str = "LOW"
+
+
